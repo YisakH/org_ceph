789f6d17dac9fc3e4c96c5013a264b8af0e0f6d1
YisakH <drm05003@naver.com>
2024-04-26 17:25:49 +0900

    H-ACL -> HBAC edited

--- util/create_user.sh
+++ util/create_user.sh
@@ -2,10 +2,13 @@
 
 users=("root" "president" "dean1" "dean2" "dean3" "chair1" "chair2" "chair3" "chair4" "chair5" "chair6" "chair7" "student1" "student2" "student3" "student4" "student5" "student6")
 
-cd /ceph/org_ceph/build/
+cur=$(dirname "$0")
+
+cd ${cur}/../build
+
 # Loop through users array to create each user
 for uid in "${users[@]}"; do
-    /ceph/org_ceph/build/bin/radosgw-admin user create --uid="$uid" --display-name="$uid" --access-key="$uid" --secret-key="$uid"
+    ./bin/radosgw-admin user create --uid="$uid" --display-name="$uid" --access-key="$uid" --secret-key="$uid"
 done
 
 

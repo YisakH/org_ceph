5c1d3f9a4c1cf8552ab924c0bff491c903785e03
YisakH <drm05003@naver.com>
2024-05-14 02:56:27 +0900

    create user bash edited

--- util/create_user.sh
+++ util/create_user.sh
@@ -3,10 +3,11 @@
 #users=("root" "president" "dean1" "dean2" "dean3" "chair1" "chair2" "chair3" "chair4" "chair5" "chair6" "chair7" "student1" "student2" "student3" "student4" "student5" "student6")
 users=("root" "president" "dean1" "dean2" "dean3" "chair1" "chair2" "chair3" "chair4" "chair5" "student1" "student2" "student3" "student4" "student5")
 
-cd /ceph/org_ceph/build/
+BASEDIR="/home/yshong/git/org_ceph"
+cd $BASEDIR/build
 # Loop through users array to create each user
 for uid in "${users[@]}"; do
-    /ceph/org_ceph/build/bin/radosgw-admin user create --uid="$uid" --display-name="$uid" --access-key="$uid" --secret-key="$uid"
+    ./bin/radosgw-admin user create --uid="$uid" --display-name="$uid" --access-key="$uid" --secret-key="$uid"
 done
 
 
